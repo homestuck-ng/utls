a237478b35ca0262416d74915924688d2c6c8d1f
wwqgtxx <wwqgtxx@gmail.com>
2025-04-12 19:48:40 +0800

    Add session id generator

--- handshake_client.go
+++ handshake_client.go
@@ -328,6 +328,25 @@ func (c *Conn) clientHandshake(ctx context.Context) (err error) {
 
 	c.serverName = hello.serverName
 
+	// [SHADOWTLS SECTION BEGINS]
+	// A random session ID is used to detect when the server accepted a ticket
+	// and is resuming a session (see RFC 5077). In TLS 1.3, it's always set as
+	// a compatibility measure (see RFC 8446, Section 4.1.2).
+	if c.config.SessionIDGenerator != nil {
+		hello.sessionId = make([]byte, 32)
+		hello.original = nil
+		data, err := hello.marshal()
+		if err != nil {
+			return err
+		}
+		err = c.config.SessionIDGenerator(data, hello.sessionId)
+		if err != nil {
+			return errors.New("tls: generate session id failed: " + err.Error())
+		}
+		hello.original = nil
+	}
+	// [SHADOWTLS SECTION ENDS]
+
 	if _, err := c.writeHandshakeRecord(hello, nil); err != nil {
 		return err
 	}
