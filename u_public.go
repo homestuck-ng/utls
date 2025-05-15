800edd4e182f9e5ae6b1dac298c3a66c7dd2de6b
Mingye Chen <mingye.chen@colorado.edu>
2025-05-15 09:43:47 +0800

    feat: generate different ecdhe key for pq key When the client sends both pq and non-pq keyshares, the ecdhe key was reused in line with stdlib. However this can be used to fingerprint utls ClientHellos. Generate different ecdhe keys instead, in line with Chrome. This will have to change when we support more browsers with different ways of handling this.

--- u_public.go
+++ u_public.go
@@ -873,9 +873,10 @@ func (kpk *kemPrivateKey) ToPublic() *KemPrivateKey {
 }
 
 type KeySharePrivateKeys struct {
-	CurveID CurveID
-	Ecdhe   *ecdh.PrivateKey
-	mlkem   *mlkem.DecapsulationKey768
+	CurveID    CurveID
+	Ecdhe      *ecdh.PrivateKey
+	mlkem      *mlkem.DecapsulationKey768
+	mlkemEcdhe *ecdh.PrivateKey
 }
 
 func (ksp *KeySharePrivateKeys) ToPrivate() *keySharePrivateKeys {
@@ -883,9 +884,10 @@ func (ksp *KeySharePrivateKeys) ToPrivate() *keySharePrivateKeys {
 		return nil
 	}
 	return &keySharePrivateKeys{
-		curveID: ksp.CurveID,
-		ecdhe:   ksp.Ecdhe,
-		mlkem:   ksp.mlkem,
+		curveID:    ksp.CurveID,
+		ecdhe:      ksp.Ecdhe,
+		mlkem:      ksp.mlkem,
+		mlkemEcdhe: ksp.mlkemEcdhe,
 	}
 }
 
@@ -894,8 +896,9 @@ func (ksp *keySharePrivateKeys) ToPublic() *KeySharePrivateKeys {
 		return nil
 	}
 	return &KeySharePrivateKeys{
-		CurveID: ksp.curveID,
-		Ecdhe:   ksp.ecdhe,
-		mlkem:   ksp.mlkem,
+		CurveID:    ksp.curveID,
+		Ecdhe:      ksp.ecdhe,
+		mlkem:      ksp.mlkem,
+		mlkemEcdhe: ksp.mlkemEcdhe,
 	}
 }
